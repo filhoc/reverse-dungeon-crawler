34eb7b835c1fbda84bd24feef79d5ddb495c6ed3
filhoc <filhoc@wsc.school.nz>
2026-09-25 12:30:15 +1200

    finished worrior

--- gordon-fight/warrior.gd
+++ gordon-fight/warrior.gd
@@ -1,7 +1,18 @@
-extends Node2D
+extends CharacterBody2D
+
+@export var speed: float = 100.0
+
+var player: CharacterBody2D
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
 
 
+func _physics_process(delta: float) -> void:
+	if player:
+		# Find the direction to the player
+		var direction = global_position.direction_to(player.global_position)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		# Move towards the player
+		velocity = direction * speed
+		move_and_slide()
