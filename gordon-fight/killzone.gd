cd2e67e0199c002830fff465f7820514ff28aeaf
filhoc <filhoc@wsc.school.nz>
2026-09-25 13:39:38 +1200

    update

--- gordon-fight/killzone.gd
+++ gordon-fight/killzone.gd
@@ -3,4 +3,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("you died")
+	print(body)
 	get_tree().reload_current_scene()
--- gordon-fight/warrior.gd
+++ gordon-fight/warrior.gd
@@ -1,18 +1,22 @@
-extends CharacterBody2D
+extends Node2D
 
 @export var speed: float = 100.0
+@export var gravity: float = 1000.0
 
 var player: CharacterBody2D
+var vertical_velocity: float = 0.0
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	if player:
-		# Find the direction to the player
+		# Follow the player left and right
 		var direction = global_position.direction_to(player.global_position)
 
-		# Move towards the player
-		velocity = direction * speed
-		move_and_slide()
+		global_position.x += direction.x * speed * delta
+
+	# Gravity
+	vertical_velocity += gravity * delta
+	global_position.y += vertical_velocity * delta
